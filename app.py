ef43c592c725829903cc765f403ee2b03b5dadd6
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2024-04-17 00:39:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 from fileinput import filename
 from flask import Flask, redirect, render_template, request, send_file, jsonify, url_for
 from flask_cors import CORS
-
-from cartoonize import generate_cartoonize_img
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
-from matplotlib import pyplot as plt
 from werkzeug.utils import secure_filename
 import base64
 import numpy as np
@@ -50,6 +50,52 @@ def uniqueImgName(imgName, isGenerated:bool):
 
         return ImgName_unique
 
+def edge_detection(img, line_width, blur_amount):
+    gray_scale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Smooting gray scale image(this will helps in better edges detection)
+
+    # In median blur, the central element of the image 
+    # .... is replaced by the median of all the pixels
+    gray_scale_img_blur = cv2.medianBlur(gray_scale_img, blur_amount)
+
+
+    # Now detecting edges
+    # Syntax: 
+    #   cv2.adaptiveThreshold(source, maxVal, adaptiveMethod, thresholdType, blocksize, constant)
+    img_edges = cv2.adaptiveThreshold(gray_scale_img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_width, blur_amount)
+
+    return img_edges
+
+def color_quantization(img, k_value, epochs, accuracy):  # k_value is number of clusters    
+    data = np.float32(img)
+    data = data.reshape((-1, 3))
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, epochs, accuracy)
+
+    # Now applying kmeans algorithm
+    # Syntax: 
+    #   cv2.kmeans(samples, nclusters(K), criteria, attempts, flags)    
+    ret, centers_position, centroid = cv2.kmeans(data, k_value, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    centroid = np.uint8(centroid)
+    result = centroid[centers_position.flatten()]
+
+    # reshaping image to its actual shape
+    result = result.reshape(img.shape)
+
+    return result
+
+
+
+def generate_cartoonize_img(img, LINE_WIDTH, BLUR_VALUE, TOTAL_COLORS, EPOCHS, ACCURACY):
+    edgeImg = edge_detection(img, LINE_WIDTH, BLUR_VALUE)
+    quantized_img = color_quantization(img, TOTAL_COLORS, EPOCHS, ACCURACY)
+
+    # bilateralFilter is used for removing noice and smothing
+    blurred_img = cv2.bilateralFilter(quantized_img, d=7, sigmaColor=200, sigmaSpace=200)
+    cartoonized_img = cv2.bitwise_and(blurred_img, blurred_img, mask = edgeImg)
+    
+    return cartoonized_img
+    
 
 
 @app.route('/')
