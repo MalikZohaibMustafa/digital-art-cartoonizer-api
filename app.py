89a80d0609109366c66b0d7b0936af53186dbc1e
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2024-04-28 23:39:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,51 +26,135 @@ ACCURACY = 0.95
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def edge_detection(img, line_width, blur_amount):
-    gray_scale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_scale_img_blur = cv2.medianBlur(gray_scale_img, blur_amount)
-    img_edges = cv2.adaptiveThreshold(gray_scale_img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_width, blur_amount)
-    return img_edges
+def update_c(C,hist):
+    while True:
+        groups=defaultdict(list)
 
-def color_quantization(img, k_value, epochs, accuracy):
-    data = np.float32(img).reshape((-1, 3))
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, epochs, accuracy)
-    _, labels, centers = cv2.kmeans(data, k_value, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    centers = np.uint8(centers)
-    result = centers[labels.flatten()].reshape(img.shape)
-    return result
+        for i in range(len(hist)):
+            if(hist[i] == 0):
+                continue
+            d=np.abs(C-i)
+            index=np.argmin(d)
+            groups[index].append(i)
+
+        new_C=np.array(C)
+        for i,indice in groups.items():
+            if(np.sum(hist[indice])==0):
+                continue
+            new_C[i]=int(np.sum(indice*hist[indice])/np.sum(hist[indice]))
+
+        if(np.sum(new_C-C)==0):
+            break
+        C=new_C
+
+    return C,groups
+
+# Calculates K Means clustering
+def K_histogram(hist):
+
+    alpha=0.001
+    N=80
+    C=np.array([128])
+
+    while True:
+        C,groups=update_c(C,hist)
+
+        new_C=set()
+        for i,indice in groups.items():
+            if(len(indice)<N):
+                new_C.add(C[i])
+                continue
+
+            z, pval=stats.normaltest(hist[indice])
+            if(pval<alpha):
+                left=0 if i==0 else C[i-1]
+                right=len(hist)-1 if i ==len(C)-1 else C[i+1]
+                delta=right-left
+                if(delta >=3):
+                    c1=(C[i]+left)/2
+                    c2=(C[i]+right)/2
+                    new_C.add(c1)
+                    new_C.add(c2)
+                else:
+                    new_C.add(C[i])
+            else:
+                new_C.add(C[i])
+        if(len(new_C)==len(C)):
+            break
+        else:
+            C=np.array(sorted(new_C))
+    return C
+
+# The main controlling function
+def caart(img):
+
+    kernel=np.ones((2,2), np.uint8)
+    output=np.array(img)
+    x,y,c=output.shape
+    for i in range(c):
+        output[:,:,i]=cv2.bilateralFilter(output[:,:,i],5,150,150)
+
+    edge=cv2.Canny(output, 100, 200)
+    output=cv2.cvtColor(output,cv2.COLOR_RGB2HSV)
+
+    hists = []
+
+    hist,_=np.histogram(output[:,:,0],bins =np.arange(180+1))
+    hists.append(hist)
+    hist,_=np.histogram(output[:,:,1],bins =np.arange(256+1))
+    hists.append(hist)
+    hist,_=np.histogram(output[:,:,2],bins =np.arange(256+1))
+    hists.append(hist)
 
 
-def generate_cartoonize_img(img, line_width, blur_value, total_colors, epochs, accuracy):
-    edges = edge_detection(img, line_width, blur_value)
-    quantized = color_quantization(img, total_colors, epochs, accuracy)
-    blurred = cv2.bilateralFilter(quantized, d=7, sigmaColor=200, sigmaSpace=200)
-    cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
-    return cartoon
+    C=[]
+    for h in hists:
+        C.append(K_histogram(h))
+    #print("centroids: {0}".format(C))
+
+    output=output.reshape((-1,c))
+    for i in range(c):
+        channel=output[:,i]
+        index=np.argmin(np.abs(channel[:, np.newaxis] - C[i]), axis=1)
+        output[:,i]=C[i][index]
+    output=output.reshape((x,y,c))
+    output=cv2.cvtColor(output, cv2.COLOR_HSV2RGB)
+
+    contours,_=cv2.findContours(edge,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(output,contours,-1,0,thickness=1)
+    #cartoon = cv2.bitwise_and(output, output, mask=contours)
+    for i in range(3):
+        output[:,:,i]=cv2.erode(output[:,:,i], kernel, iterations=1)
+    #Laplacian = cv2.Laplacian(output,cv2.CV_8U, ksize=11)
+    #output=output-Laplacian
+    return output
 
 
 @app.route('/')
 def home():    
     return render_template('index.html')
-
+    
 @app.route('/submit', methods=['POST'])
-def submit():
+def upload_file():
     if 'img' not in request.files:
-        return jsonify({'message': 'No file part'}), 400
+        return jsonify({'error': 'No file part'}), 400
     file = request.files['img']
     if file.filename == '':
-        return jsonify({'message': 'No image selected for uploading'}), 400
+        return jsonify({'error': 'No selected file'}), 400
     if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # Read the image file in a format that can be processed
         image = Image.open(file.stream)
-        img_np = np.array(image)
-        cartoon_img = generate_cartoonize_img(img_np, LINE_WIDTH, BLUR_VALUE, TOTAL_COLORS, EPOCHS, ACCURACY)
-        cartoon_pil = Image.fromarray(cartoon_img)
+        img_np = np.array(image.convert('RGB'))
+        output_image = caart(img_np)  # Process the image
+        # Convert the output image to send as a response
+        img_pil = Image.fromarray(output_image)
         img_io = io.BytesIO()
-        cartoon_pil.save(img_io, 'JPEG', quality=70)
+        img_pil.save(img_io, 'JPEG', quality=85)
         img_io.seek(0)
-        return send_file(img_io, mimetype='image/jpeg', download_name='cartoonized.jpeg')
+        return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='cartoonized.jpeg')
     else:
-        return jsonify({'message': 'Allowed image types are png, jpg, jpeg'}), 400
+        return jsonify({'error': 'Allowed image types are png, jpg, jpeg'}), 400
 
 
 if __name__ == "__main__":
